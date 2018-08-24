39bd0401099d8dffa1eb019ef71739fce6ccfd9a
Jose Chu del Barco <jorgechud@bcp.com.pe>
2018-08-24 16:28:13 -0500

    breaking captcha

---
+++ src/breaking-captcha.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+DATA = '../img'
+
+print(os.getcwd())
+
+img = cv2.imread(f'{DATA}/captcha2.PNG')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+bilateral = cv2.bilateralFilter(gray, 3, 75, 75)
+ret, thresh = cv2.threshold(
+    bilateral,
+    25,
+    255,
+    cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+# Kernel
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+# other things
+erosion = cv2.erode(thresh, kernel, iterations=1)
+closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel, iterations=1)
+
+# Transform image
+dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 5)
+ret, sure_fg = cv2.threshold(
+    dist_transform,
+    0.02*dist_transform.max(),
+    255,
+    cv2.THRESH_BINARY)
+# ,255,0)
+
+# kernel_1
+kernel_1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 2))
+
+dilation_1 = cv2.dilate(sure_fg, kernel_1, iterations=2)
+erosion_1 = cv2.erode(dilation_1, kernel_1, iterations=3)
+
+plt.imshow(erosion_1, 'gray')
+cv2.waitKey(0)
+cv2.destroyAllWindows()
