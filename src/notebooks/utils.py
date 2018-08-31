38fd180e8ac0a5a36b5f28bb8f8155e6c45204fd
jchudb93 <jchudb93@gmail.com>
2018-08-31 14:46:45 -0500

    utils

--- src/notebooks/utils.py
+++ src/notebooks/utils.py
@@ -47,9 +47,48 @@ def get_captcha(driver, element, path):
     image = Image.open(path)
 
     left = location['x']
-    top = location['y'] + 140
+    top = location['y']
     right = location['x'] + size['width']
-    bottom = location['y'] + size['height'] + 140
+    bottom = location['y'] + size['height']
 
     image = image.crop((left, top, right, bottom))  # defines crop points
-    image.save(path, 'jpeg')  # saves new cropped image
+    image.save(path, 'PNG')  # saves new cropped image
+
+def transform_image(image_path, output_filename='image', img_format = 'PNG'):
+    
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    bilateral = cv2.bilateralFilter(gray, 3, 75, 75)
+    ret, thresh = cv2.threshold(
+        bilateral,
+        25,
+        255,
+        cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    # Kernel
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    # other things
+    erosion = cv2.erode(thresh, kernel, iterations=1)
+    closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel, iterations=1)
+
+    # Transform image
+    dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(
+        dist_transform,
+        0.02*dist_transform.max(),
+        255,
+        cv2.THRESH_BINARY)
+    # ,255,0)
+
+    # kernel_1
+    kernel_1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 2))
+
+    dilation_1 = cv2.dilate(sure_fg, kernel_1, iterations=2)
+    erosion_1 = cv2.erode(dilation_1, kernel_1, iterations=3)
+
+    cv2.imshow("img", erosion_1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    
